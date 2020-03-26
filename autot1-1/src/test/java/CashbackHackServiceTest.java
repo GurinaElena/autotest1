98af8ddd24fdff09368d5babb76c9be91c8b5a1b
Gurina Elena <gurina_elena@inbox.ru>
2020-03-26 15:08:29 +0300

    Проведение тестов для второй версии сервиса

--- autot1-1/src/test/java/CashbackHackServiceTest.java
+++ autot1-1/src/test/java/CashbackHackServiceTest.java
@@ -13,7 +13,6 @@ class CashbackHackServiceTest {
         int expected = 100;
 
         assertEquals(expected, actual);
-
     }
 
     @Test
@@ -22,20 +21,15 @@ class CashbackHackServiceTest {
         int amount = 1000;
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
-
         assertEquals(expected, actual);
 
     }
-
     @Test
     void shouldReturn400IfAmountIs1600 (){
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1600;
         int actual = cashbackHackService.remain(amount);
         int expected = 400;
-
         assertEquals(expected, actual);
-
     }
-
 }
